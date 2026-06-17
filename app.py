7d64582650434975635cdbf82af19df046342c95
AviDhandhania <dhandhaniaavi@gmail.com>
2026-06-17 15:23:29 +0530

    Used Code Formatters + Some layout.html

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template, request, session
 from cs50 import SQL
 from flask_session import Session
 
-#configure application
+# configure application
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "your_secret_key_here"
@@ -10,20 +10,19 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
-
 db = SQL("sqlite:///chat.db")
 
 
-@app.route('/')
+@app.route("/")
 def index():
     if not session.get("user_id"):
         return redirect("/login")
-    
+
     rooms = db.execute("SELECT * FROM rooms ORDER BY name ASC")
-    return render_template("index.html", rooms = rooms)
+    return render_template("index.html", rooms=rooms)
 
 
-@app.route("/login", methods = ["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     session.clear()
     if request.method == "POST":
@@ -31,11 +30,13 @@ def login():
             return render_template("todo.html")
         if not request.form.get("password"):
             return render_template("todo.html")
-    return 
+    return
 
-@app.route("/register", methods = ["GET", "POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     return "<h1>register<h1>"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
