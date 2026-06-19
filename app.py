a969830d573017ca9adae5c5a0565ca82138bfe7
AviDhandhania <dhandhaniaavi@gmail.com>
2026-06-19 10:59:45 +0530

    register path integration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, render_template, request, session
 from cs50 import SQL
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required
 
 # configure application
 app = Flask(__name__)
@@ -14,6 +16,7 @@ db = SQL("sqlite:///chat.db")
 
 
 @app.route("/")
+@login_required
 def index():
     if not session.get("user_id"):
         return redirect("/login")
@@ -22,20 +25,48 @@ def index():
     return render_template("index.html", rooms=rooms)
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    session.clear()
-    if request.method == "POST":
-        if not request.form.get("username"):
-            return render_template("todo.html")
-        if not request.form.get("password"):
-            return render_template("todo.html")
-    return
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     session.clear()
+#     if request.method == "POST":
+#         if not request.form.get("username"):
+#             return render_template("todo.html")
+#         if not request.form.get("password"):
+#             return render_template("todo.html")
+#     return
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return "<h1>register<h1>"
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return apology("must provide username", 403)
+        if not password:
+            return apology("must provide password", 403)
+        if not confirmation:
+            return apology("must provide confirmation", 403)
+
+        if password != confirmation:
+            return apology("passwords must match", 403)
+
+        if db.execute("SELECT * FROM users WHERE username=?", username):
+            return apology("username already exists", 402)
+
+        user_id = db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)",
+            username,
+            generate_password_hash(password),
+        )
+
+        session["user_id"] = user_id
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
 
 
 if __name__ == "__main__":
